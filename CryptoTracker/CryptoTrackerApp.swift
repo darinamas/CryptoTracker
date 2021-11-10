ec9d5f5d57d65b0e36aa5fb1c66c73282a9767cb
Darina <daryna.massive@gmail.com>
2021-11-10 15:55:57 +0100

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Daryna Polevyk on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
